d0ad2adb58e09e9c324735714d2d9ff66bb045a7
amirhossein Abd <thegrolam@gmail.com>
2024-10-09 10:36:11 +0330

    fix: requirements gruut farsi installation fixed

--- requirements.sh
+++ requirements.sh
@@ -1,4 +1,4 @@
 #!bin/bash
 pip install -i https://mirror-pypi.runflare.com/simple coqui-tts==0.24.0 --no-cache-dir
 echo 'installing persian phonemeizer.\nsing & wait ...'
-install -f 'https://synesthesiam.github.io/prebuilt-apps/' gruut[fa] --no-cache-dir
+pip install -f 'https://synesthesiam.github.io/prebuilt-apps/' gruut[fa] --no-cache-dir
